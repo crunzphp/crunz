0de662fd9249e892e62e7fcb8204963c0f66ffee
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2022-08-15 08:06:28 +0200

    Extend "\Symfony\Component\Console\Command\Command" directly

--- src/Console/Command/ScheduleListCommand.php
+++ src/Console/Command/ScheduleListCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ScheduleListCommand extends Command
+class ScheduleListCommand extends \Symfony\Component\Console\Command\Command
 {
     /** @var ConfigurationInterface */
     private $configuration;
